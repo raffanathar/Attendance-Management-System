3571b777d7fd918229ef7a71fc6f78bfef806312
raffan <rao.affan2003@gmail.com>
2024-11-21 22:59:02 +0500

    affan

--- Attendence Management System/LeaveManager.cpp
+++ Attendence Management System/LeaveManager.cpp
@@ -1,1 +1,89 @@
 #include "LeaveManager.h"
+
+void LeaveManager::addLeave(Leave *leave)
+{
+    leaves.push_back(leave);
+}
+
+vector<Leave *> LeaveManager::getLeaves() const
+{
+    return leaves;
+}
+
+void LeaveManager::approveLeave(const string &empId, const string &leaveType)
+{
+  Supervisor supervisor;
+  Director director;
+
+  if (leaveType == "Casual" && isexceedingthreasholdest(empId,leaveType)) 
+  {
+    cout<<"cant take more than 4 Casual Leaves "<<endl;
+    supervisor.approveLeave(*this,empId,leaveType);
+    director.approveLeave(*this,empId, leaveType);
+  }
+
+  if (leaveType == "Earned"){
+    supervisor.approveLeave(*this,empId,leaveType);
+    director.approveLeave(*this,empId, leaveType);
+  }
+  else if (leaveType == "Official"){
+    supervisor.approveLeave(*this,empId,leaveType);
+  }
+  else if (leaveType == "Unpaid")
+  {
+    supervisor.approveLeave(*this,empId,leaveType);
+  }
+  else if (leaveType == "Casual"){
+    
+    cout<<"Approved"<<endl;
+  }
+  else {
+    cout<<"Invalid Leave Type"<<endl;
+  }
+  
+}
+
+vector<Leave *> LeaveManager::getPendingLeave() const
+{
+    vector<Leave *> pendingLeaves;
+    for (const auto &leave : leaves) {
+        if (leave->getstatus() == "Pending") {
+            pendingLeaves.push_back(leave);
+        }
+    }
+    return pendingLeaves;
+}
+
+vector<Leave *> LeaveManager::getApprovedLeave() const
+{
+    vector<Leave *> approvedLeaves;
+    for (const auto &leave : leaves) {
+        if (leave->getstatus() == "Approved") {
+            approvedLeaves.push_back(leave);
+        }
+    }
+    return approvedLeaves;
+
+}
+
+vector<Leave *> LeaveManager::getLeavesbyempid(const string &empid) const
+{
+    vector<Leave *> leavesByEmpId;
+    for (const auto &leave : leaves) {
+        if (leave->getempId() == empid) {
+            leavesByEmpId.push_back(leave);
+        }
+    }
+    return leavesByEmpId;
+}
+
+bool LeaveManager::isexceedingthreasholdest(const string &empid, const string &leaveType) const
+{
+    int leaveCount = 0;
+    for (Leave* leave : leaves) {
+        if (leave->gettype() == leaveType && leave->getstatus() == "Approved" && leave->getstatus() == empid) {
+            leaveCount++;
+        }
+    }
+    return leaveCount >= 4;
+}
--- Attendence Management System/LeaveManager.h
+++ Attendence Management System/LeaveManager.h
@@ -1,19 +1,25 @@
 #ifndef LEAVEMANAGER_HPP
 #define LEAVEMANAGER_HPP
 
+#include<iostream>
 #include "Leave.h"
+#include "Supervisor.h"
+#include "Director.h"
 #include <vector>
 using namespace std;
 
 class LeaveManager {
 private:
-    vector<Leave*> leaves;  // Stores all leave applications
+    vector<Leave*> leaves;  
 
 public:
     void addLeave(Leave* leave);
     vector<Leave*> getLeaves() const;
     void approveLeave(const string& empId, const string& leaveType);
-    void rejectLeave(const string& empId, const string& leaveType);
+    vector<Leave*> getPendingLeave() const;
+    vector<Leave*> getApprovedLeave() const;
+    vector<Leave*> getLeavesbyempid(const string& empid) const;
+    bool isexceedingthreasholdest(const string& empid, const string& leaveType) const;
 };
 
 #endif
